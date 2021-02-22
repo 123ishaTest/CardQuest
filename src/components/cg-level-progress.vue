2163dd0dfaa527c764b2b7420443e92a08c91ae5
Isha <ishadijcks@gmail.com>
2021-02-22 03:01:07 +0100

    Show level progress relatively to current turn

--- src/components/cg-level-progress.vue
+++ src/components/cg-level-progress.vue
@@ -5,7 +5,7 @@
     </p>
     <div class="flex flex-col">
       <div :key=futureCard[0] v-for="futureCard in futureCards" class="my-2">
-        <div>{{ futureCard[0] }} - {{ futureCard[1].title }}
+        <div>{{ futureCard[0] - currentTurn }} - {{ futureCard[1].title }}
           <img :title="futureCard[1].description" class="w-8 h-8 inline"
                :src="require(`@/assets/cards/${futureCard[1].image}`)">
         </div>
