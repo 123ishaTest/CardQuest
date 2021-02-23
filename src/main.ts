6700dfc1c60d8499099bac46965e6093e2ae3b2c
Isha <ishadijcks@gmail.com>
2021-02-24 00:29:17 +0100

    Remove fontawesome reference

--- src/main.ts
+++ src/main.ts
@@ -14,14 +14,6 @@ declare global {
     }
 }
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faUserSecret)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
 
 
