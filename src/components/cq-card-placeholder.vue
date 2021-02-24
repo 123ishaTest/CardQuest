112938f079469b23a20e2c1a6dc694c4a285bbd0
Isha <ishadijcks@gmail.com>
2021-02-24 23:38:36 +0100

    Keep empty spots in hand

---
+++ src/components/cq-card-placeholder.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="playable-card" class="w-48 h-72 p-2 m-2 border-2 flex flex-col">
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: "cq-card-placeholder",
+}
+</script>
+
