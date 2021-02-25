3aeba78ad88482d4987f5ab69533f04a35e49c64
Isha <ishadijcks@gmail.com>
2021-02-25 02:42:16 +0100

    Fix type error

--- src/components/cq-super-powers.vue
+++ src/components/cq-super-powers.vue
@@ -6,16 +6,16 @@
 </template>
 
 <script>
-
-
 import CqSuperPower from "@/components/cq-super-power";
+import {SuperPowers} from "@/card-quest/adventure/super-powers/SuperPowers";
+
 export default {
   name: "cq-super-powers",
   components: {CqSuperPower},
 
   props: {
     powers: {
-      type: Array,
+      type: SuperPowers,
       required: true
     },
   },
