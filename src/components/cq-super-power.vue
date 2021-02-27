70543610e29391aa4f5b1f908db9aee50579bd74
Isha <ishadijcks@gmail.com>
2021-02-27 17:17:12 +0100

    Fix super power display

--- src/components/cq-super-power.vue
+++ src/components/cq-super-power.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="flex-1">
     <div class="flex flex-col items-center m-2">
       <p class="text-2xl text-center mx-2">
         {{ power.valueText }} {{ power.description }}
