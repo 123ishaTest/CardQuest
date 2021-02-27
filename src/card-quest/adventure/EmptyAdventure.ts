5d2abb3aa5ff76e367d30228201742253d32df6b
Isha <ishadijcks@gmail.com>
2021-02-27 12:04:18 +0100

    Fix EmptyAdventure

--- src/card-quest/adventure/EmptyAdventure.ts
+++ src/card-quest/adventure/EmptyAdventure.ts
@@ -4,10 +4,11 @@ import {Level} from "@/card-quest/adventure/Level";
 import {PlayerStats} from "@/card-quest/adventure/PlayerStats";
 import {LevelId} from "@/card-quest/adventure/LevelId";
 import {CardPackId} from "@/card-quest/features/card-collection/CardPackId";
+import {ImpossibleRequirement} from "@/ig-template/tools/requirements/ImpossibleRequirement";
 
 export class EmptyAdventure extends Adventure {
 
     constructor() {
-        super(new Deck(), new Level(-1 as LevelId, '', '', -1 as CardPackId, 0, 0, []), new PlayerStats(5, 10));
+        super(new Deck(), new Level(-1 as LevelId, '', '', -1 as CardPackId, 0, 0, [], new ImpossibleRequirement()), new PlayerStats(5, 10));
     }
 }
