8f9a4dd1da5a6cc70097ff0f16a6b00c1e06f507
Isha <ishadijcks@gmail.com>
2021-02-27 16:51:27 +0100

    Add save buttons to settings

--- src/components/settings/igt-settings.vue
+++ src/components/settings/igt-settings.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="feature-tab">
+    <div class="flex flex-row">
+      <button class="btn btn-red" @click="resetSave">Reset save</button>
+      <button class="btn btn-blue" @click="save">Save</button>
+    </div>
     <igt-boolean-setting :setting=saveSetting></igt-boolean-setting>
- </div>
+  </div>
 </template>
 
 <script>
-
+import {App} from "@/App.ts"
 import {Settings} from "@/ig-template/features/settings/Settings";
 import IgtBooleanSetting from "@/components/settings/igt-boolean-setting";
 import {SettingId} from "@/ig-template/features/settings/SettingId";
@@ -13,6 +17,12 @@ import {SettingId} from "@/ig-template/features/settings/SettingId";
 export default {
   name: "igt-settings",
   components: {IgtBooleanSetting},
+  data() {
+    return {
+      game: App.game
+    }
+  },
+
   props: {
     settings: {
       type: Settings,
@@ -20,11 +30,24 @@ export default {
     },
   },
 
+  methods: {
+    resetSave() {
+      if (confirm("Are you sure you want to delete your save? This does not provide you with bonuses")) {
+        this.game.deleteSave();
+        location.reload();
+      }
+    },
+    save() {
+      this.game.save();
+    }
+  },
+
   computed: {
     saveSetting() {
       return this.settings.getSetting(SettingId.ShowSaveMessage);
     }
-  },
+  }
+  ,
 }
 </script>
 
