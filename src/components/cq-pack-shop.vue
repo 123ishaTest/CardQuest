a818fbf72c94abd4f12b12358d3dfb0d9b46c0f0
Isha <ishadijcks@gmail.com>
2021-02-28 17:24:54 +0100

    Fix card pack preview opacity

--- src/components/cq-pack-shop.vue
+++ src/components/cq-pack-shop.vue
@@ -2,19 +2,20 @@
   <div class="my-4">
     <div class="flex flex-row flex-wrap">
 
-      <button @click="buyPack(pack.id)" class="btn btn-blue flex flex-col justify-between items-center has-tooltip"
-              v-for="pack in buyablePacks"
-              :key="pack.id"
-              :disabled="!canAfford(pack)">
-        <span>{{ pack.id }}</span>
-        <span class="flex flex-row mx-2">
+      <div v-for="pack in buyablePacks" :key="pack.id" class="has-tooltip">
+        <button @click="buyPack(pack.id)" class="btn btn-blue flex flex-col justify-between items-center"
+
+                :disabled="!canAfford(pack)">
+          <span>{{ pack.id }}</span>
+          <span class="flex flex-row mx-2">
           {{ -pack.cost.amount }}
             <img class="mx-1 h-6 w-6" :src="require(`@/assets/coins.svg`)" alt="coins.svg">
 
             </span>
-        <cq-card-pack-preview  :pack-id="pack.id" class="tooltip mt-12"></cq-card-pack-preview>
 
-      </button>
+        </button>
+        <cq-card-pack-preview :pack-id="pack.id" class="tooltip mt"></cq-card-pack-preview>
+      </div>
     </div>
   </div>
 </template>
