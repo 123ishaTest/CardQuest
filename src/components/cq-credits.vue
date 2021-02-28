bc4235ce5387f3043b09d0b9122309db5178e28e
Isha <ishadijcks@gmail.com>
2021-02-28 17:59:29 +0100

    Update credits

--- src/components/cq-credits.vue
+++ src/components/cq-credits.vue
@@ -7,12 +7,17 @@
     <div class="p-4">
       <p class="text-lg">This game was Developed in 7 days by <a href="http://github.com/ishadijcks/" target="_blank">Isha</a>
         for the <a href="https://incremental_games.reddit.com/" target="_blank">r/incremental_games</a> jam of 2021.</p>
+      <p class="text-lg">You can view the Source Code on <a href="https://github.com/123ishaTest/CardQuest"
+                                                            target="_blank">GitHub</a>.</p>
+
       <p class="text-lg">It uses the <a href="https://github.com/123ishaTest/incremental-game-template" target="_blank">Incremental
         Game Template</a>.</p>
       <p class="text-lg">I realize it isn't the most traditional incremental game, but it should still be enjoyable
         :)</p>
       <br>
+      <br>
       <p class="text-lg">Thank you for playing, I hope you liked it.</p>
+
     </div>
     <div class="p-4">
       <p class="text-lg">Artwork taken from <a href="https://game-icons.net/" target="_blank">game-icons.net</a></p>
